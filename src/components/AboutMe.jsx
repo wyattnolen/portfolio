9c5bc1319e343c92d2d082227492272400259c81
Wyatt Nolen <wyattnolen@gmail.com>
2024-11-03 14:34:20 -0500

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,26 +2,34 @@ const AboutMe = () => {
 	return (
 		<section className="line-relax">
 			<p className="mb-4">
-				Back in 2012, I decided to try my hand at creating custom Tumblr themes
-				and tumbled head first into the rabbit hole of coding and web
-				development. Fast-forward to today, and I’ve had the privilege of
-				building software for an advertising agency, a start-up, a huge
-				corporation, and a digital product studio.
+				Back in 2012, I took my first steps into web development at a small
+				agency called Zavoodi, and I've been hooked ever since. Starting with
+				custom Tumblr themes, I quickly discovered a passion for front-end
+				development and have spent the last decade honing my skills. Along the
+				way, I've had the opportunity to work at an advertising agency, a
+				start-up, a large corporation, and a digital product studio. Each
+				experience has deepened my commitment to creating high-quality web
+				experiences, from large-scale applications to finely crafted components.
 			</p>
 
 			<p className="mb-4">
-				My main focus these days is building accessible user interfaces for our
-				customers at Klaviyo. I most enjoy building software in the sweet spot
-				where design and engineering meet — things that look good but are also
-				built well under the hood. In my free time, I've also released an online
-				video course that covers everything you need to know to build a web app
-				with the Spotify API.
+				Today, my work centers on building high-performance websites with a
+				strong emphasis on accessibility and visual appeal. I'm passionate about
+				designing and coding at the intersection of aesthetics and
+				functionality, ensuring that each user interface I create is intuitive,
+				responsive, and accessible to everyone. I'm always exploring ways to
+				bring creativity to my projects, from crafting seamless user experiences
+				to enhancing the finer details that make an interface feel polished and
+				complete.
 			</p>
 
 			<p className="mb-4">
-				When I'm not at the computer, I'm usually rock climbing, reading,
-				hanging out with my wife and two cats, or running around Hyrule
-				searching for Korok seeds
+				When I'm not coding, I'm usually busy raising my daughter, Clark, and
+				finding balance in activities I love. You can often find me in the
+				garden, experimenting with composting, or unwinding with my wife while
+				we play Baldur's Gate 3. I'm always drawn to projects that let me
+				combine creativity with technical skill, and I love bringing that energy
+				into my work and personal life alike.
 			</p>
 		</section>
 	);
