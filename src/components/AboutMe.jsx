c0f30d8e227086ef31f712ce026029c0a2a71f20
Wyatt Nolen <wyattnolen@gmail.com>
2024-11-03 21:05:15 -0500

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,34 +2,17 @@ const AboutMe = () => {
 	return (
 		<section className="line-relax">
 			<p className="mb-4">
-				Back in 2012, I took my first steps into web development at a small
-				agency called Zavoodi, and I've been hooked ever since. Starting with
-				custom Tumblr themes, I quickly discovered a passion for front-end
-				development and have spent the last decade honing my skills. Along the
-				way, I've had the opportunity to work at an advertising agency, a
-				start-up, a large corporation, and a digital product studio. Each
-				experience has deepened my commitment to creating high-quality web
-				experiences, from large-scale applications to finely crafted components.
+				Back in 2007, I took my first steps into web development when I tried my hand at developing MySpace themes, which sparked my passion for front-end development. Since then, I've gained experience across diverse environments, from a <a className="text-primary" target="_blank" href="https://zavoodi.com/">start-up</a>, a web consulting studio, a newspaper, to several mid-market e-commerce companies in industries such as <a className="text-primary" target="_blank" href="https://www.doversaddlery.com/">horse lifestyle</a>, <a className="text-primary" target="_blank" href="https://www.halegroves.com/">food & gifts</a>, <a className="text-primary" target="_blank" href="https://www.vandykes.com/">furniture</a>, <a className="text-primary" target="_blank" href="https://www.agrisupply.com/">tractor supply</a>, <a className="text-primary" target="_blank" href="https://pureromance.com/">health & beauty</a>, and <a className="text-primary" target="_blank" href="https://www.blackwidowpro.com/">hardware</a>.
 			</p>
 
 			<p className="mb-4">
-				Today, my work centers on building high-performance websites with a
-				strong emphasis on accessibility and visual appeal. I'm passionate about
-				designing and coding at the intersection of aesthetics and
-				functionality, ensuring that each user interface I create is intuitive,
-				responsive, and accessible to everyone. I'm always exploring ways to
-				bring creativity to my projects, from crafting seamless user experiences
-				to enhancing the finer details that make an interface feel polished and
-				complete.
+				These days, my focus is threefold: building accessible user interfaces,
+				optimizing website performance, and transforming designs to be modern
+				and engaging. But sometimes I just want to want to <a className="text-primary" target="_blank" href="https://codepen.io/wyattnolen/pen/zYPWrdg">build fun things</a>.
 			</p>
 
 			<p className="mb-4">
-				When I'm not coding, I'm usually busy raising my daughter, Clark, and
-				finding balance in activities I love. You can often find me in the
-				garden, experimenting with composting, or unwinding with my wife while
-				we play Baldur's Gate 3. I'm always drawn to projects that let me
-				combine creativity with technical skill, and I love bringing that energy
-				into my work and personal life alike.
+            When I'm not coding, I'm usually wrapped up in the joy of raising my daughter, Clark, and savoring moments that bring balance to life. You'll often find me in the garden, exploring composting experiments, or unwinding with my wife as we adventure together in Baldur's Gate 3.
 			</p>
 		</section>
 	);
